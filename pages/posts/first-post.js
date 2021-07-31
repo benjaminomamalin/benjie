f3a14a70606cd6f1a4e844aff290bd06a7b639d3
Benjamin <benjamin.omamalin@gmail.com>
2021-07-31 21:49:12 +0800

    :tada: init

---
+++ pages/posts/first-post.js
@@ -0,0 +1,21 @@
+// Then, open pages/posts/first-post.js, add an import for the Layout component, and make it the outermost component:
+
+import Layout from '../../components/layout'
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default function FirstPost() {
+    return (
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+      </Layout>
+    )
+  }
